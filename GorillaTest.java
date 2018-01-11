d5c85fee05057fa192c5e2f520d4cf4e4591d2b3
Ricardo <torrez.ricky7@gmail.com>
2018-01-11 18:31:46 -0500

    initiall commit

---
+++ GorillaTest.java
@@ -0,0 +1,13 @@
+public class GorillaTest {
+    public static void main(String args[]){
+        Gorilla g = new Gorilla();
+        
+        g.throwItem();
+        g.throwItem();
+        g.throwItem();
+        g.eatBananas();
+        g.eatBananas();
+        g.climb();
+        g.displayEnergy();
+    }
+}
